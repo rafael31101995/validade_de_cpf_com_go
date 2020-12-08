ec141c8f77d47becbab455754140e39718049def
rafael <rafael.cds.monteiro@hotmail.com>
2020-12-08 16:20:48 -0300

    Adicionando iteraçao ao todosnumerosiguais

--- valida_cpf.go
+++ valida_cpf.go
@@ -2,16 +2,18 @@ package main
 
 import "fmt"
 
+// Retorna se o CPF é um CPF valido ou não.
 func validaCpf(cpf string) bool {
-	bVal := false
+	bVal := todosNumerosIguais(cpf)
 	return bVal
 }
 
+// Certifica-se de que o CPF não é um CPF com todos os números iguais.
 func todosNumerosIguais(cpf string) bool {
-	for i := 0; i < 5; i++ {
-		fmt.Println(i)
+	for i := 0; i < len(cpf); i++ {
+		fmt.Printf("%c\n", cpf[i])
 	}
-	return true
+	return false
 }
 
 func main() {
